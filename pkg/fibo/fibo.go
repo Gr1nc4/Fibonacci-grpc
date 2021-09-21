12789ddd1a9663e17014ce707b47d8a2e843f269
Gr1nc4 <78788051+Gr1nc4@users.noreply.github.com>
2021-09-21 17:22:07 +0700

    (after deadline)

--- pkg/fibo/fibo.go
+++ pkg/fibo/fibo.go
@@ -8,16 +8,16 @@ import (
 type GRPCServer struct {}
 
 func (s *GRPCServer) Fibo(ctx context.Context, req *api.FiboReq)(*api.FiboResp, error){
-	return &api.FiboResp{Result: Fib(req.GetX())}, nil
+	return &api.FiboResp{Result: FiboSlice( req.GetX(), req.GetY())}, nil
 }
 var (
-	cache map[int32]int32
+	cache map[int64]int64
 )
 func init(){
-	cache = make(map[int32]int32)
+	cache = make(map[int64]int64)
 }
 //Calculate fibonacci number
-func Fib(n int32) int32{
+func Fib(n int64) int64{
 	if n <= 1{
 		return n
 	}
@@ -27,4 +27,11 @@ func Fib(n int32) int32{
 	sum := Fib(n-1) + Fib(n-2)
 	cache[n] = sum
 	return sum
+}
+func FiboSlice(x,y int64) []int64 {
+	var arr []int64
+	for i := x; i <y ; i++ {
+		arr = append(arr, Fib(i))
+	}
+	return arr
 }
