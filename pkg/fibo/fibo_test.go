55afb8cc11b17eb489f991e335b934c8b4d1b629
Gr1nc4 <78788051+Gr1nc4@users.noreply.github.com>
2021-09-21 17:30:46 +0700

    (after deadline)

--- pkg/fibo/fibo_test.go
+++ pkg/fibo/fibo_test.go
@@ -5,11 +5,25 @@ import (
 )
 
 func TestFib(t *testing.T){
-	var n int32 = 10
-	var out int32 = 55
+	var n int64 = 10
+	var out int64 = 55
 
 	result := Fib(n)
+
 	if result != out{
-		t.Error("Incorrect result", result,  " not equal ", out)
+		t.Error("Incorrect result", result, "not equal", out)
+	}
+}
+
+func TestFiboSlice(t *testing.T) {
+	var x, y int64
+	x, y = 0, 3
+	var arr = FiboSlice(x, y)
+	var arrTest = []int64{0, 1, 1, 2}
+
+	for i := 0; i < len(arr); i++ {
+		if arr[i] != arrTest[i] {
+			t.Error("Incorrect result", arr[i], "not equal", arrTest[i])
+		}
 	}
 }
